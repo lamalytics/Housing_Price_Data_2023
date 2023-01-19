70bd938abe576ca0ee99225e0d7c318855ba4b0a
lamalytics <95007715+lamalytics@users.noreply.github.com>
2023-01-18 21:12:56 -0800

    Updated with XGB Regressor model

--- Housing.py
+++ Housing.py
@@ -3,7 +3,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 # from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import OneHotEncoder
-from xgboost import XGBRegressor
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error as MSE
 
 train_set = pd.read_csv(
     "house-prices-advanced-regression-techniques-data/train.csv")
@@ -28,36 +29,34 @@ test_set = pd.read_csv(
 train_set.drop(columns=["PoolQC", "MiscFeature", "Alley", "Fence", "FireplaceQu"], inplace=True)
 # print(train_set.isnull().sum().sort_values(ascending=False))
 
+# scaling categorical variables
+# NEED to scale numerical values to normalize
+features = train_set.drop(columns=["SalePrice"])
+target = np.array(train_set.loc[:,["SalePrice"]])
+categorical_cols = features.select_dtypes(include=["object"])
+train_set_encoded = pd.get_dummies(features, columns=categorical_cols.columns)
+final_features = np.array(train_set_encoded)
 
-pre_train_set, post_train_set = train_test_split(train_set, test_size=0.2, random_state=21)
-pre_train_set = pd.DataFrame(pre_train_set)
+# partitition training set since test_set has no sales price for prediction
+train_features, test_features, train_target, test_target = train_test_split(final_features,target, test_size=0.2, random_state=21)
 
-features = pre_train_set.drop(columns=["SalePrice"])
-target = pre_train_set.loc[:,["SalePrice"]]
+# print(train_target)
 
-encoder = OneHotEncoder(handle_unknown='ignore')
-features_encoded = encoder.fit_transform(features.select_dtypes(include=["object"]))
-# print(features_encoded)
-features_numeric = features.select_dtypes(exclude=["object"])
-# print(features_numeric)
+# select only categorical columns
+# categorical_cols = features.select_dtypes(include=["object"]).columns
+# test_cat_cols = test_features.select_dtypes(include=["object"]).columns
+# encoding cat variables as dummies on pre_train set
+# train_set_encoded = pd.get_dummies(features, columns=categorical_cols)
+# test_set_encoded = pd.get_dummies(test_features, columns=test_cat_cols)
+# features_final = train_set_encoded.values
 
-final_features = np.concatenate(features_encoded.toarray(), features_numeric, axis=1)
-xgb = XGBRegressor(enable_categorical=True)
-# # scaler on this
-# xgb.fit(features,target)
+xgb_model = xgb.XGBRegressor()
+# make target from DF to a 1D array 
+# xgb_model.fit(features_final, target.values.ravel())
+xgb_model.fit(train_features, train_target)
+predict_target = xgb_model.predict(test_features)
 
-# pred_target = xgb.predict(features)
+rmse = MSE(test_target, predict_target) ** (1/2)
+print(rmse)
+# RMSE at 33K :(
 
-# print(pred_target)
-
-# test_set.drop(columns=["PoolQC", "MiscFeature", "Alley", "Fence", "FireplaceQu"], inplace=True)
-# print(test_set.isnull().sum().sort_values(ascending=False))
-# print(test_set.head())
-
-
-
-# # Fit the imputer on the data
-# imputer.fit(train_set)
-
-# # Apply the imputer to the data
-# df_imputed = imputer.transform(train_set)
