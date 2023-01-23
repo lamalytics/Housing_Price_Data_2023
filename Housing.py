f84faa01209a2ce21781be2019154b577705fa37
lamalytics <95007715+lamalytics@users.noreply.github.com>
2023-01-22 23:45:46 -0800

    Update Housing.py

--- Housing.py
+++ Housing.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
-# from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
 import xgboost as xgb
 from sklearn.metrics import mean_squared_error as MSE
 
@@ -21,21 +22,37 @@ test_set = pd.read_csv(
 # print(train_set.describe())
 # print(test_set.describe())
 
+sns.histplot(data=train_set, x="SalePrice", color="orange")
+sns.displot(data=train_set, x="MSZoning", color="blue")
+
+
+corr_matrix = train_set.corr(method='spearman', numeric_only=True)
+sns.heatmap(corr_matrix, cmap='coolwarm')
+plt.show()
+
 # print(train_set.isnull().sum().sort_values(ascending=False))
 # print(test_set.isnull().sum().sort_values(ascending=False))
 
 # some are categorical; some numerical
 # imputer = SimpleImputer(strategy='mean')  # you can also use median or mode
-train_set.drop(columns=["PoolQC", "MiscFeature", "Alley", "Fence", "FireplaceQu"], inplace=True)
+train_set.drop(columns=["PoolQC", "MiscFeature", "Alley", "Fence", "FireplaceQu", "Id"], inplace=True)
 # print(train_set.isnull().sum().sort_values(ascending=False))
 
 # scaling categorical variables
 # NEED to scale numerical values to normalize
 features = train_set.drop(columns=["SalePrice"])
 target = np.array(train_set.loc[:,["SalePrice"]])
+
+# select numerical cols and categorical for scaling
+numerical_cols = features.select_dtypes(include=["float64", "int64"]).columns
 categorical_cols = features.select_dtypes(include=["object"])
+
+# scaling numerical and replace numerical cols with scaled nums
+min_max_scaler = MinMaxScaler()
+features[numerical_cols] = min_max_scaler.fit_transform(features[numerical_cols].values)
 train_set_encoded = pd.get_dummies(features, columns=categorical_cols.columns)
-final_features = np.array(train_set_encoded)
+# combine the scaled numerical features and dummy encoded categorical features
+final_features = np.concatenate((features[numerical_cols].values, train_set_encoded.values), axis=1)
 
 # partitition training set since test_set has no sales price for prediction
 train_features, test_features, train_target, test_target = train_test_split(final_features,target, test_size=0.2, random_state=21)
@@ -50,9 +67,8 @@ train_features, test_features, train_target, test_target = train_test_split(fina
 # test_set_encoded = pd.get_dummies(test_features, columns=test_cat_cols)
 # features_final = train_set_encoded.values
 
+# next step hypertuning
 xgb_model = xgb.XGBRegressor()
-# make target from DF to a 1D array 
-# xgb_model.fit(features_final, target.values.ravel())
 xgb_model.fit(train_features, train_target)
 predict_target = xgb_model.predict(test_features)
 
